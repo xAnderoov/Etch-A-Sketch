075820bd2e56438682baaa4a44ca42c4decc23c1
Alexander <psashv@gmail.com>
2023-04-28 17:53:29 +0300

    change color when 'hovering', add size change button

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
 
+const gridSize = 16;
 const createEl = className => {
   const element = document.createElement('div');
   element.classList.add(className);
   return element;
 }
-
 const createRow = size => {
   const row = createEl('row');
   for (let i = 0; i < size; i++) {
@@ -13,7 +13,6 @@ const createRow = size => {
   }
   return row;
 }
-
 const createSquare = size => {
   if (size > 100) size = 100;
   const square = createEl('square');
@@ -22,5 +21,19 @@ const createSquare = size => {
   }
   return square;
 }
+const mouseHandler = e => {
+  e.target.style['background-color'] = 'black';
+  e.target.removeEventListener('mouseenter', mouseHandler);
+}
+const init = size => {
+  document.querySelector('main').appendChild(createSquare(size));
 
-document.querySelector('main').appendChild(createSquare(16));
+  Array.from(document.querySelectorAll('.cell')).forEach(cell => {
+    cell.addEventListener('mouseenter', mouseHandler);
+  })
+}
+document.querySelector('.button--size').addEventListener('click', () => {
+  document.querySelector('.square').remove();
+  init(prompt('Please type grid size:', 16));
+})
+init(gridSize);
